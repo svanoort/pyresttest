ee2d27eca45768a07a562405cf4431cb8d2b09bf
Sam Van Oort <samvanoort@gmail.com>
2014-06-03 21:55:15 -0400

    Set maintainer and add dependencies to distutils config

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.core import setup
 setup(name='pyresttest',
     version='0.1',
     description='Python Rest Testing',
-    maintainer='Naveen Malik',
-    maintainer_email='jewzaam@gmail.com',
+    maintainer='Sam Van Oort',
+    maintainer_email='acetonespam@gmail.com',
     url='https://github.com/svanoort/pyresttest',
-    py_modules=['resttest','pycurl_benchmark','test_resttest'],
-    license='Apache License, Version 2.0'
+    py_modules=['resttest','test_resttest'],
+    license='Apache License, Version 2.0',
+    requires=['argparse','yaml','pycurl']
     )
