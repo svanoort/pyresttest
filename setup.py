645bdb78725d6597dd7e90eced2a2780cfa44e61
Sam Van Oort <samvanoort@gmail.com>
2014-11-09 13:07:41 -0500

    Finish preparing for PyPi submission
    Done: Verified pip install/uninstall works from test PyPi instance correctly

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from distutils.core import setup
 
 setup(name='pyresttest',
-    version='0.1',
+    version='1.0',
     description='Python RESTful API Testing & Microbenchmarking Tool',
     long_description='Python RESTful API Testing & Microbenchmarking Tool',
     maintainer='Sam Van Oort',
     maintainer_email='acetonespam@gmail.com',
     url='https://github.com/svanoort/pyresttest',
-    keywords='rest web http testing',
+    keywords=['rest', 'web', 'http', 'testing'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Utilities'
+    ],
     py_modules=['pyresttest.resttest','pyresttest.generators','pyresttest.binding',
         'pyresttest.parsing', 'pyresttest.validators', 'pyresttest.contenthandling',
         'pyresttest.benchmarks','pyresttest.tests'],
