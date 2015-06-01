5006738b53b987d7dd067c231e72e0805129faac
Ben Lau <blau@mashery.com>
2015-06-01 09:41:30 -0700

    clean up internal naming for new curl options

--- pyresttest/resttest.py
+++ pyresttest/resttest.py
@@ -263,9 +263,9 @@ def run_test(mytest, test_config = TestConfig(), context = None):
     body = StringIO()
     curl.setopt(pycurl.WRITEDATA, body)
     curl.setopt(pycurl.HEADERFUNCTION, headers.write)
-    if test_config.curl_verbose:
+    if test_config.verbose:
         curl.setopt(pycurl.VERBOSE,True)
-    if test_config.curl_insecure_ssl:
+    if test_config.ssl_insecure
         curl.setopt(pycurl.SSL_VERIFYPEER,0)
         curl.setopt(pycurl.SSL_VERIFYHOST,0)
 
@@ -699,11 +699,11 @@ def main(args):
         if 'interactive' in args and args['interactive'] is not None:
             t.config.interactive = safe_to_bool(args['interactive'])
 
-        if 'curl_verbose' in args and args['curl_verbose'] is not None:
+        if 'verbose' in args and args['verbose'] is not None:
             t.config.curl_verbose = safe_to_bool(args['curl_verbose'])
 
-        if 'curl_insecure_ssl' in args and args['curl_insecure_ssl'] is not None:
-            t.config.curl_insecure_ssl = safe_to_bool(args['curl_insecure_ssl'])
+        if 'ssl_insecure' in args and args['ssl_insecure'] is not None:
+            t.config.ssl_insecure = safe_to_bool(args['ssl_insecure'])
 
     # Execute all testsets
     failures = run_testsets(tests)
@@ -720,8 +720,8 @@ def command_line_run(args_in):
     parser.add_option(u"--test", help="Test file to use", action="store", type="string")
     parser.add_option(u'--import_extensions', help='Extensions to import, separated by semicolons', action="store", type="string")
     parser.add_option(u'--vars', help='Variables to set, as a YAML dictionary', action="store", type="string")
-    parser.add_option(u'--verbose', help='Put cURL into verbose mode for extra debugging power', action='store_true', default=False, dest="curl_verbose")
-    parser.add_option(u'--ssl-insecure', help='Disable cURL host and peer cert verification', action='store_true', default=False, dest="curl_insecure_ssl")
+    parser.add_option(u'--verbose', help='Put cURL into verbose mode for extra debugging power', action='store_true', default=False, dest="verbose")
+    parser.add_option(u'--ssl-insecure', help='Disable cURL host and peer cert verification', action='store_true', default=False, dest="ssl_insecure")
 
     (args, unparsed_args) = parser.parse_args(args_in)
     args = vars(args)
