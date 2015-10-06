45c4caa010d1af0dd69400cce79960c7d5ebc3df
Sam Van Oort <samvanoort@gmail.com>
2015-10-05 21:18:22 -0400

    Enhance image build scripts to test docker images

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,17 @@
-#!/bin/bash
-docker build -t pyresttest-build-ubuntu-14:0.1 -f Dockerfile-ubuntu-14 .
+# Build
+cd docker
+docker build -t pyresttest-build-ubuntu-14:0.1-SNAPSHOT -f Dockerfile-ubuntu-14 .
+docker build -t pyresttest-build-centos6:0.1-SNAPSHOT -f Dockerfile-centos6 .
 
-docker build -t pyresttest-build-centos6:0.1 -f Dockerfile-centos6 .
+# Test images and if they pass, tag appropriately
+docker run -it --rm pyresttest-build-ubuntu-14:0.1-SNAPSHOT python /tmp/verify_image.py
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+docker tag pyresttest-build-ubuntu-14:0.1-SNAPSHOT pyresttest-build-ubuntu-14:0.1
+
+docker run -it --rm pyresttest-build-centos6:0.1-SNAPSHOT python /tmp/verify_image.py
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+docker tag pyresttest-build-centos6:0.1-SNAPSHOT pyresttest-build-centos6:0.1
