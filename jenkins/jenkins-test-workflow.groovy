906cb16923a427fdc34bba5c839973caecc89485
Sam Van Oort <samvanoort@gmail.com>
2015-10-11 10:53:34 -0400

    Refactor docker build/test environments
    Slim down the python3 image
    Make use of latest tag in docker
    Restructure docker folders for Jenkins use

--- jenkins/jenkins-test-workflow.groovy
+++ jenkins/jenkins-test-workflow.groovy
@@ -1,10 +1,10 @@
-def testEnv = docker.image('pyresttest-build-ubuntu-14:0.1')
-def testEnv26 = docker.image('pyresttest-build-centos6:0.1')
+def testEnv = docker.image('pyresttest-build-ubuntu-14:latest')
+def testEnv26 = docker.image('pyresttest-build-centos6:latest')
 
 // Run unit/functional/additional tests on given image
 def doTest(imageName, unitTestCommand, functionalTestCommand, additionalTestScript) {
   imageName.inside() {
-    git url: "$repo", branch: "$branch"
+
     dir('pyresttest') {
       sh unitTestCommand
       sh functionalTestCommand
@@ -13,21 +13,19 @@ def doTest(imageName, unitTestCommand, functionalTestCommand, additionalTestScri
   }
 }
 
-// Define the environments and specific test syntax for each
-def envRuns = [:]
-envRuns['ubuntu-python27'] = {node {
+node {
+  // Run in one node, but use docker multiple times with same repo
+  git url: "$repo", branch: "$branch"
+  stage 'Unit Test ubuntu-python27'
   doTest(testEnv, "python -m unittest discover -s pyresttest -p 'test_*.py'",  'python pyresttest/functionaltest.py', 'bash test_use_extension.sh')
-}}
 
-envRuns['centos6-python26'] = {node {
+  stage 'Unit Test centos6-python26'
   doTest(testEnv26, "python -m discover -s pyresttest -p 'test_*.py'",  'python pyresttest/functionaltest.py', 'bash test_use_extension.sh')
-}}
+}
 
 // Test Python 3 support, which *currently does not work*
+// def testEnvPython3 = docker.image('pyresttest-build-centos6:latest')
 //envRuns['ubuntu-python3'] = {node {
-//  doTest(testEnv, "python3 -m discover -s pyresttest -p 'test_*.py'",  'python3 pyresttest/functionaltest.py',
+//  doTest(testEnvPython, "python3 -m discover -s pyresttest -p 'test_*.py'",  'python3 pyresttest/functionaltest.py',
 //     "python3 pyresttest/resttest.py https://api.github.com extension_use_test.yaml --import_extensions 'sample_extension'")
 //}}
-
-parallel envRuns
-
