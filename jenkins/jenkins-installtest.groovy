f607e9e9761bbb7bddfeca32f069bf3dd3574513
Sam Van Oort <samvanoort@gmail.com>
2015-10-11 16:55:54 -0400

    Installer test script yet again

--- jenkins/jenkins-installtest.groovy
+++ jenkins/jenkins-installtest.groovy
@@ -2,25 +2,29 @@ def testEnv = docker.image('pyresttest-build-ubuntu-14:latest')
 def testEnv26 = docker.image('pyresttest-build-centos6:latest')
 def centos = docker.image('centos:centos6')
 def ubuntu = docker.image('ubuntu:14.04')
+def args = '-u root'  //We need this to allow installs inside container
 
 def headlesstests() {
     // Tests that only require python/pip and a pyresttest installation, no django server
     echo 'Running headless test'
-    dir('/tmp') {
-        sh 'python -c "import pyresttest"'
-        sh 'resttest.py'
-        sh 'pyresttest'
-    }
-    sh "resttest.py https://github.com simple_test.yaml"
+
+    // The dir command causes a hangup *sigh*
+    //dir('/tmp') {
+    //    sh 'python -c "import pyresttest"'
+        // Check command will run, but capture error code since help will also return error code
+        sh "resttest.py 2>/dev/null | grep 'Usage' "
+        sh "pyresttest 2>/dev/null | grep 'Usage' "  //TODO check for proper text, catch error code 2
+//    }
+    sh "resttest.py https://github.com simple_test.yaml" // FAILS: we need a better test YAML
 }
 
 def servertests() {
     // Tests that require a full test server running to execute
-    dir ('/tmp') {
+    //dir ('/tmp') {
         echo 'Running server test'
         sh "python pyresttest/testapp/manage.py testserver test_data.json &"
         sh "pyresttest pyresttest/content-test.yaml"
-    }
+    //}
 }
 
 def clean_workspace() {
@@ -32,11 +36,9 @@ def clean_workspace() {
 
 node {
     git url:'https://github.com/svanoort/pyresttest.git', branch:'master'
-    stage name:'basic installation', concurrency:1
-
 
     // Test easyinstall, etc installation of scripts
-    testEnv.inside() {
+    testEnv.inside(args) {
         clean_workspace()
         sh 'python setup.py install'
         headlesstests()
@@ -44,15 +46,15 @@ node {
     }
 
     // Test standard local build/install
-    testEnv26.inside() {
+    testEnv26.inside(args) {
         clean_workspace()
-        sh 'python setup.py bdist_rpm' // Standard install
+        sh 'python setup.py install' // Standard install
         headlesstests()
         servertests()
     }
 
      // Test RPM build/install
-    testEnv26.inside() {
+    testEnv26.inside(args) {
         clean_workspace()
         sh 'python setup.py bdist_rpm'
         stash includes: 'dist/*.noarch.rpm', name: 'rpm-py26'
@@ -62,7 +64,7 @@ node {
     }
 
     stage name:'Publish to test PyPi', concurrency:1
-    testEnv.inside() {
+    testEnv.inside(args) {
         clean_workspace()
         // Requires credentials and credentials binding plugin
         // Uses a secret stored pypirc file with pypitest enabled
@@ -72,10 +74,10 @@ node {
         }
     }
 
-    stage name:'Test PyPy installation', concurrency:1
+    stage name:'Test PyPy installation'
 
     // Smoketest pip installation in a naked CentOS image
-    centos.inside() {
+    centos.inside(args) {
         clean_workspace()
         sh 'rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
         sh 'yum install -y python-pip'
@@ -84,7 +86,7 @@ node {
     }
 
     // Smoketest RPM installation in a naked CentOS image
-    centos.inside() {
+    centos.inside(args) {
         clean_workspace()
         dir ("/tmp") {
             unstash name: 'rpm-py26'
@@ -94,7 +96,7 @@ node {
     }
 
     // Smoketest pip installation inside a naked ubuntu 14 image
-    ubuntu.inside() {
+    ubuntu.inside(args) {
         clean_workspace()
         sh 'apt-get update && apt-get install -y python-pip'
         sh 'pip install -i https://testpypi.python.org/pypi pyresttest'
@@ -102,14 +104,14 @@ node {
     }
 
     // Functional test using integrated server
-    testEnv.inside() {
+    testEnv.inside(args) {
         clean_workspace()
         sh 'pip install -i https://testpypi.python.org/pypi pyresttest'
         servertests()
     }
 
     // Functional test using integrated server
-    testEnv26.inside() {
+    testEnv26.inside(args) {
         clean_workspace()
         sh 'pip install -i https://testpypi.python.org/pypi pyresttest'
         servertests()
