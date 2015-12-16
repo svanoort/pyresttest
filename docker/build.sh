d6c44f697faed078f35c55a6eb6fd8a8747d7d1b
Sam Van Oort <samvanoort@gmail.com>
2015-12-15 21:40:44 -0500

    Fix the docker build script for Jenkins use by not forcing interactive mode

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,7 @@
-# Build
+#!/bin/bash
+# Build and verify docker images for testing/developing pyresttest
+
+set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
@@ -11,27 +14,30 @@ docker build -t pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT ./centos6-py2
 docker build -t pyresttest-build-python3:$PYTHON3_VERSION-SNAPSHOT ./python3
 
 # Test images and if they pass, tag appropriately
-docker run -it --rm pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION-SNAPSHOT python /tmp/verify_image.py
+docker run -t --rm pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION-SNAPSHOT python /tmp/verify_image.py
 if [ $? -ne 0 ]; then  # Test failed, remove the built image and exit with error
     docker rmi pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION-SNAPSHOT
+    echo 'Ubuntu 14 build with python 2.7 failed'
     exit 1
 fi
 docker tag -f pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION-SNAPSHOT pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION
 docker tag -f pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION-SNAPSHOT pyresttest-build-ubuntu-14:latest
 docker rmi pyresttest-build-ubuntu-14:$UBUNTU_14_VERSION-SNAPSHOT
 
-docker run -it --rm pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT python /tmp/verify_image.py
+docker run -t --rm pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT python /tmp/verify_image.py
 if [ $? -ne 0 ]; then  # Test failed, remove the built image and exit with error
     docker rmi pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT
+    echo 'CentOS6 build with python 2.6 failed'
     exit 1
 fi
 docker tag -f pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT pyresttest-build-centos6:$CENTOS6_VERSION
 docker tag -f pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT pyresttest-build-centos6:latest
 docker rmi pyresttest-build-centos6:$CENTOS6_VERSION-SNAPSHOT
 
-docker run -it --rm pyresttest-build-python3:$PYTHON3_VERSION-SNAPSHOT python3 /tmp/verify_image.py
+docker run -t --rm pyresttest-build-python3:$PYTHON3_VERSION-SNAPSHOT python3 /tmp/verify_image.py
 if [ $? -ne 0 ]; then  # Test failed, remove the built image and exit with error
     docker rmi pyresttest-build-python3:$PYTHON3_VERSION-SNAPSHOT
+    echo 'Debian-wheezy build with python 3.4.3 failed'
     exit 1
 fi
 docker tag -f pyresttest-build-python3:$PYTHON3_VERSION-SNAPSHOT pyresttest-build-python3:0.2
