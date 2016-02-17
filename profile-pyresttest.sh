38a3f552c10e2b01fbda17bf0f0d6efbda1ccd4a
Sam Van Oort <samvanoort@gmail.com>
2016-02-16 21:47:46 -0500

    Revise profiling of content test

--- profile-pyresttest.sh
+++ profile-pyresttest.sh
@@ -3,14 +3,14 @@ python -m cProfile -s cumtime pyresttest/resttest.py http://jsonplaceholder.typi
 
 # Use the Django test app to benchmark
 python pyresttest/testapp/manage.py testserver pyresttest/testapp/test_data.json &
-DJANGO_PROCESS=$?
+DJANGO_PROCESS=$!
 sleep 5
-python -m cProfile -s tottime pyresttest/resttest.py http://localhost:8000 pyresttest/content-test.yaml > contenttest.txt
+python -m cProfile -s tottime pyresttest/resttest.py http://localhost:8000 pyresttest/content-test.yaml > contenttest-tot.txt
 kill -9 $DJANGO_PROCESS
 
 # Clean slate for cumulative call time
 python pyresttest/testapp/manage.py testserver pyresttest/testapp/test_data.json &
-DJANGO_PROCESS=$?
+DJANGO_PROCESS=$!
 sleep 5
-python -m cProfile -s cumtime pyresttest/resttest.py http://localhost:8000 pyresttest/content-test.yaml > contenttest.txt
+python -m cProfile -s cumtime pyresttest/resttest.py http://localhost:8000 pyresttest/content-test.yaml > contenttest-cum.txt
 kill -9 $DJANGO_PROCESS
