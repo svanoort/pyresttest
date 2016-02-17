e4a7ea0741ed8fa003303adecb6e0732f840fea4
Sam Van Oort <samvanoort@gmail.com>
2016-02-16 21:26:05 -0500

    Add profiling script for test

---
+++ profile-pyresttest.sh
@@ -0,0 +1,16 @@
+python -m cProfile -s tottime pyresttest/resttest.py http://jsonplaceholder.typicode.com jsontest.yaml > jsontest-tots.txt
+python -m cProfile -s cumtime pyresttest/resttest.py http://jsonplaceholder.typicode.com jsontest.yaml > jsontest-cum.txt
+
+# Use the Django test app to benchmark
+python pyresttest/testapp/manage.py testserver pyresttest/testapp/test_data.json &
+DJANGO_PROCESS=$?
+sleep 5
+python -m cProfile -s tottime pyresttest/resttest.py http://localhost:8000 pyresttest/content-test.yaml > contenttest.txt
+kill -9 $DJANGO_PROCESS
+
+# Clean slate for cumulative call time
+python pyresttest/testapp/manage.py testserver pyresttest/testapp/test_data.json &
+DJANGO_PROCESS=$?
+sleep 5
+python -m cProfile -s cumtime pyresttest/resttest.py http://localhost:8000 pyresttest/content-test.yaml > contenttest.txt
+kill -9 $DJANGO_PROCESS
